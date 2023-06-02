906bf21c7bb920e150f500f12c903f0476991c80
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-06-02 10:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function clcik() {
+  let fd = document.createElement("div");
+  fd.id = "browser-info";
+  fd.innerText = `You are using ${navigator.appName} version ${navigator.appVersion}`;
+
+  document.body.append(fd);
+}
+
+clcik();
+
